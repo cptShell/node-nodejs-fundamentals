bcc16a72771829509b11db954ccae4b9043950d8
cptShell <yauhenimaslousky@gmail.com>
2026-03-03 14:03:56 +0300

    feat: add restore script

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,41 @@
+import path from "path";
+import fs from "fs/promises";
+
+const SNAPSHOT_FILE_NAME = "snapshot.json";
+const RESTORED_DIR_NAME = "workspace_restored";
+const ENCODING_TYPE = { BASE64: "base64", UTF8: "utf-8" };
+const READ_FILE_CONFIG = { encoding: ENCODING_TYPE.UTF8 };
+
+const projectRoot = process.cwd();
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const snapshotPath = path.join(projectRoot, SNAPSHOT_FILE_NAME);
+
+  try {
+    await fs.access(snapshotPath);
+  } catch {
+    console.error('Snapshot не найден по пути:', snapshotPath);
+    return;
+  }
+
+  const snapshotData = await fs.readFile(snapshotPath, READ_FILE_CONFIG);
+  const snapshot = JSON.parse(snapshotData);
+  const restoredRoot = path.join(projectRoot, RESTORED_DIR_NAME);
+
+  await fs.mkdir(restoredRoot, { recursive: true });
+
+  snapshot.entries.forEach(async (entry) => {
+    const targetPath = path.join(restoredRoot, entry.path);
+    if (entry.type === "directory") {
+      await fs.mkdir(targetPath, { recursive: true });
+    } else if (entry.type === "file") {
+      const dir = path.dirname(targetPath);
+      await fs.mkdir(dir, { recursive: true });
+
+      const buffer = Buffer.from(entry.content, ENCODING_TYPE.BASE64);
+      await fs.writeFile(targetPath, buffer);
+    }
+  });
 };
 
 await restore();
